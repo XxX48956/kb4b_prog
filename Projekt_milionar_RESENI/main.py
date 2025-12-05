9fe858802721d0a6e0290c2c5995cdfb383b0f23
unknown <CL2-09@jcekb.cz>
2025-12-05 13:19:55 +0100

    chaos

---
+++ Projekt_milionar_RESENI/main.py
@@ -0,0 +1,16 @@
+exit = False
+users = []
+quizQuestionsPath = "Projekt_milionar_RESENI\files\quiz_questions.csv"
+authPath = "Projekt_milionar_RESENI\files\users.txt"
+winnersPath = "Projekt_milionar_RESENI\files\winners.txt"
+
+def main():
+    while not exit:
+        print("Program běží...")
+
+main()  
+
+
+
+if __name__ == "__main__":
+    main()
--- Projekt_milionar_RESENI/milionar.py
+++ Projekt_milionar_RESENI/milionar.py
@@ -1,28 +1,21 @@
 
-exit = False
-users = []
-quizQuestionsPath = "Projekt_milionar_RESENI\files\quiz_questions.csv"
-authPath = "Projekt_milionar_RESENI\files\users.txt"
-winnersPath = "Projekt_milionar_RESENI\files\winners.txt"
+
+
+
 
 class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.register_to_txt()
 
     def register_to_txt(self):
         with open(authPath, "a", encoding="utf-8") as file:
-            file.write(f"{self.username}: {self.password}")
+            file.write(f"{self.username}: {self.password}\n")
 
 
     def __del__(self):
-        print(f"Objekt se jmenem {self.username} byl smazán")
-
-
-def main():
-    isAuth = auth()
-    while not exit:
-        print("")
+        print(f"Objekt se jménem {self.username} byl smazán")
 
 
 def login():
@@ -30,17 +23,32 @@ def login():
     while not exit:
         pass
 
+def username_txt_check(username):
+    # Kontrola existujícího uživatelského jména
+    with open(authPath, "r", encoding="utf-8") as file:
+        existing_users = [line.split(": ")[0] for line in file.readlines()]
+
+    if username in existing_users:
+        print("Uživatelské jméno již existuje! Zkuste jiné.")
+        return False  
+    return True 
+
 
 def register():
     print("Registration:")
     username = input("Enter your username: \n>:")
+
+    if not username_txt_check(username):
+        return False
+
     password = input("Enter your password: \n>:")
     users.append(User(username, password))
+    
+    print(f"Uživatel {username} byl úspěšně zaregistrován.")
+    return True
+    
 
     
-# def registration_to_txt(username, password):
-#     with open(authPath, "a", encoding="utf-8") as file:
-#         file.write(f"{username}: {password}")
 
 def auth():
     choice = authMenu()
@@ -52,12 +60,13 @@ def auth():
         exit = True
     else:
         print("Invalid choice. \nChoose in range 1-3")
-        authMenu()
+        return auth()
 
 
 def authMenu():
-    while not exit:
-        print("User Authentication Menu")
+    print("User Authentication Menu")
+    
+    while 1:
         print("[1] Login")
         print("[2] Register")
         print("[3] Exit")
@@ -68,3 +77,15 @@ def authMenu():
           print("Invalid input. \nChoose number 1-3")
         
         return choice
+
+
+
+
+
+
+def load_users(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        print("Authentication file not found.")
