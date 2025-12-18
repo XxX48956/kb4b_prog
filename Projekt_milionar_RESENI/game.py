c2836b817d631f3c9ac58f76af97b8a829dd443e
rob <tvuj@email.cz>
2025-12-18 23:13:55 +0100

    game.py semi-final

--- Projekt_milionar_RESENI/game.py
+++ Projekt_milionar_RESENI/game.py
@@ -1,22 +1,13 @@
+from csv import DictReader
+from random import choice
 from vars import QUIZ_QUESTIONS_PATH, WINNERS_PATH
 from winners import update_winner_stats
-import csv
-import random
     
 
-def game_loop(user):
-    print("Lets begin the game!")
-    print("\n")
-
-    game = Game(user)
-    game.play()
-
-
 def get_valid_choice(min_value, max_value):
-    msg = f"Choose an option: [{min_value}-{max_value}]"
     while True:
         try:
-            print(msg)
+            print(f"Choose an option: [{min_value}-{max_value}]")
             choice = int(input(">:"))
             if min_value <= choice <= max_value:
                 return choice
@@ -26,20 +17,19 @@ def get_valid_choice(min_value, max_value):
             print(f"Invalid input. Choose number from {min_value} to {max_value}.")
 
 
-
-
 class Question:
     def __init__(self, data):
         self.question_text = data["question"]
-        self.correct_answer = data["correct_answer"].lower()
-        self.difficulty = data["difficulty"].lower()
+        self.correct_answer = data["correct_answer"].strip().lower
+        self.difficulty = data["difficulty"].strip().lower
 
 
 class Game:
-    QUESTION_COUNT = 3
+    QUESTION_COUNT = 15
     DIFFICULTY_LEVELS = ["easy", "medium", "hard"]
     QUESTIONS_PER_LEVEL = round(QUESTION_COUNT / len(DIFFICULTY_LEVELS))
     HINTS_AVAILABLE = ["Call a friend", "Public opinion", "50/50"]
+    
     POSITIVE_RESPONSES = ["y", "yes", "t", "true"]
     NEGATIVE_RESPONSES = ["n", "no", "f", "false"]
 
@@ -54,22 +44,13 @@ class Game:
         return self.DIFFICULTY_LEVELS[self.current_level_index]
 
 
-    def load_questions(self):
+    def load_all_questions(self, path = QUIZ_QUESTIONS_PATH):
         try:
-            with open(QUIZ_QUESTIONS_PATH, "r", encoding="utf-8") as file:
-                reader = csv.DictReader(file)
-
-                all_questions = [Question(line) for line in reader]
-                if not all_questions:
-                    print("Error: No questions in file.")
-                    return []
-                return all_questions
+            with open(path, "r", encoding="utf-8") as file:
+                return [Question(line) for line in DictReader(file)] 
 
         except FileNotFoundError:
-            print(f"Error: File with questions '{QUIZ_QUESTIONS_PATH}' not found.")
-            return []
-        except Exception:
-            print(f"Error: An error occurred while reading the file '{QUIZ_QUESTIONS_PATH}'.")
+            print(f"Error: File with questions '{path}' not found.")
             return []
 
 
@@ -80,7 +61,7 @@ class Game:
             print(f"Warning: No questions found for difficulty '{self.current_difficulty()}'.")
             return None 
             
-        return random.choice(level_questions)
+        return choice(level_questions)
 
 
     def update_difficulty(self):
@@ -90,54 +71,36 @@ class Game:
                 print(f"\n New difficulty: **{self.current_difficulty().upper()}**! ")
 
 
-    def valid_response(self, response, correct_answer):
-        if correct_answer == "true":
-            if response.lower() in self.POSITIVE_RESPONSES:
-                return True
-            elif response.lower() in self.NEGATIVE_RESPONSES:
-                return False
-            else:
-                return None
-        elif correct_answer == "false":
-            if response.lower() in self.NEGATIVE_RESPONSES:
-                return True
-            elif response.lower() in self.POSITIVE_RESPONSES:
-                return False
-            else:
-                return None
+    def check_answer(self, response, correct_answer):
+        if correct_answer in self.POSITIVE_RESPONSES:
+            return response in self.POSITIVE_RESPONSES
+        if correct_answer in self.NEGATIVE_RESPONSES:
+            return response in self.NEGATIVE_RESPONSES
+        return response == correct_answer
+    
 
-
-    def call_a_friend(self):
-        print(f"\nCall a friend? Sure… if you actually had a friend to call.")
-
-
-    def public_opinion(self):
-        print(f"\nPublic opinion? Great… too bad no one is listening.")
-
-
-    def fifty_fifty(self):
-        print(f"\n50/50? Sure, a hint worthy of a strategy master.")
-
-
-    def get_hint(self):
-        print(f"\nAvailable hints: ")
-        
-        for index, hint in enumerate(self.hints):
-            print(f"[{index + 1}] {hint}")
-
-        hint_choice = get_valid_choice(1, len(self.hints))
-
-        if self.hints[hint_choice - 1] == "Call a friend":
-            self.call_a_friend()
-            self.hints.pop(hint_choice - 1)
-        elif self.hints[hint_choice - 1] == "Public opinion":
-            self.public_opinion()
-            self.hints.pop(hint_choice - 1)
-        elif self.hints[hint_choice - 1] == "50/50":
-            self.fifty_fifty()
-            self.hints.pop(hint_choice - 1)
-        else:
+    def use_hint(self):
+        if not self.hints:
+            print("\nNo more hints")
             return
+
+        print("\n--- Available hints ---")
+        for index, hint in enumerate(self.hints, 1):
+            print(f"[{index}] {hint}")
+        
+        choice = get_valid_choice(1, len(self.hints)) - 1
+        selected_hint = self.hints.pop(choice)
+
+        messages = {
+            "Call a friend": "Call a friend? Sure… if you actually had a friend to call.",
+            "Public opinion": "Public opinion? Great… too bad no one is listening.",
+            "50/50": "50/50? Sure, a hint worthy of a strategy master."
+        }
+
+        if selected_hint in messages:
+            print(f"\n{messages[selected_hint]}")
+        else:
+            print("Hint not found.")
 
 
     def get_response(self):
@@ -158,26 +121,36 @@ class Game:
         print(f"Question: {question.question_text}")
         
         print(f"\nRemaining hints: ['hint']\n\t{', '.join(self.hints) if self.hints else 'None'}")
-        
-        response = self.get_response()
-        if response == "quit":
-            return None
-            
-        is_correct = self.valid_response(response, question.correct_answer)
-        
-        if is_correct:
-            print("Correct answer.")
-        else:
-            print(f"Wrong answer. The correct answer is **{question.correct_answer}**.")
-            return None
-            
-        return is_correct
+        while True:
+            response = input("Your answer: [or 'hint' / 'quit']").strip().lower()
+
+            if response in ['quit', 'q', 'exit', 'e']:
+                return False
+
+            if response in ['h', 'hint', 'hints']:
+                self.use_hint()
+                print(f"\n Question: {question.question_text}")
+                continue
+
+            if response in self.POSITIVE_RESPONSES or response in self.NEGATIVE_RESPONSES:
+                is_correct = self.check_answer(response, question.correct_answer)
+                self.user.answered_questions += 1
+
+                if is_correct:
+                    print("Correct answer.")
+                    self.user.correct_answers += 1
+                    return True
+                else:
+                    print(f"Wrong answer. The correct answer is **{question.correct_answer}**.")
+                    return False
+
+            print("Invalid response. Please enter 'y' for yes, 'n' for no, 'hint' for a hint, or 'quit' to exit the game.")
 
 
     def play(self):
-        print("Game begins! Good luck!")
-        
-        questions = self.load_questions() # Initialization
+        print("--- Game begins! Good luck! ---")
+        self.user.games += 1
+        questions = self.load_all_questions() # Initialization
         if not questions:
             return
 
@@ -201,7 +174,18 @@ class Game:
             print(f"\n **Congratulation!**")
             print(f"You have answered all {self.QUESTION_COUNT} questions correctly and won the game!**")
 
-            update_winner_stats(self.user) # Update winner stats
+            self.user.wins += 1
+            update_winner_stats(self.user, WINNERS_PATH) # Update winner stats
+
+
+def game_introduction(user):
+    print(f"Welcome, {user.username}!")
+    print("Let's play the čichna millionaire game.")
+    print(f"You have to answer {Game.QUESTION_COUNT} questions.")
+    print(f"With every serie of questions difficulty will increase.")
+    print("You have only one chance, one wrong answer and its over.")
+    print("Good luck!")
+    print("\n")
 
 
 def display_rules():
@@ -217,17 +201,6 @@ def display_hints():
     for hint in Game.HINTS_AVAILABLE:
         print(f"- {hint}")
     print()
-
-
-
-def game_introduction(user):
-    print(f"Welcome, {user.username}!")
-    print("Let's play the čichna millionaire game.")
-    print(f"You have to answer {Game.QUESTION_COUNT} questions.")
-    print(f"With every serie of questions difficulty will increase.")
-    print("You have only one chance, one wrong answer and its over.")
-    print("Good luck!")
-    print("\n")
 
 
 def game_main_menu(user):
@@ -253,5 +226,3 @@ def game_main_menu(user):
             break
         else:
             print("Error: Invalid choice.")
-
-
